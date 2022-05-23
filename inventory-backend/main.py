8549e2898cb0c1605bb29b98e07f14ca603ff787
linomp <lm2p@hotmail.es>
2022-05-23 20:09:14 +0200

    add CORS middleware , to try to solve problem with Flutter client

--- inventory-backend/main.py
+++ inventory-backend/main.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from dependencies import db
 from schemas.test_message import TestMessage
 
@@ -9,6 +11,16 @@ from controllers import users
 
 app = FastAPI()
 app.include_router(users.router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
