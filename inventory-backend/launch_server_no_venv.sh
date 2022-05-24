1bb25c2718ab3a423758d9b1b6e87bc4f7b58f11
linomp <lm2p@hotmail.es>
2022-05-24 11:02:23 +0200

    add shell scripts for test/launch for venv/no venv

---
+++ inventory-backend/launch_server_no_venv.sh
@@ -0,0 +1,9 @@
+pip install -r requirements.txt
+
+export HOST=165.227.107.127
+export MYSQL_USER=root
+export MYSQL_PASSWORD=09d25e094faa6ca
+export MYSQL_DATABASE=plcs_db
+export MYSQL_PORT=3306
+
+uvicorn main:app --reload
