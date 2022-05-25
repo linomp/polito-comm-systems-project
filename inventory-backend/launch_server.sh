921e1ad0146f2001e9752185fa13145ff066b88d
linomp <lm2p@hotmail.es>
2022-05-25 16:23:09 +0200

    adjust db port in scripts

--- inventory-backend/launch_server.sh
+++ inventory-backend/launch_server.sh
@@ -1,7 +1,7 @@
 source venv/Scripts/activate
 pip install -r requirements.txt
 
-export HOST=165.227.107.127
+export MYSQL_HOST=165.227.107.127
 export MYSQL_USER=root
 export MYSQL_PASSWORD=09d25e094faa6ca
 export MYSQL_DATABASE=plcs_db
